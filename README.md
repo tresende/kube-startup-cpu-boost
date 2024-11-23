a8b85d59374e530393871b4e0f7e5244f4b84d74
Thiago Resende <thiago.gcresende@gmail.com>
2024-11-23 16:30:42 -0300

    add demo app

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # kube-startup-cpu-boost
+
+
+1- build app image
+```bash
+docker build . -t tresende/calculator-api
+```
+
+2- run app image
+```bash
+docker run -p 8080:8080 --rm tresende/calculator-api
+```
+
---
+++ calculator-api/src/main/kotlin/com/tresende/calculatorapi/controller/CalculatorController.kt
@@ -0,0 +1,17 @@
+package com.tresende.calculatorapi.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class CalculatorController {
+
+    //docker run -p 8080:8080 --rm tresende/calculator-api
+    //http://localhost:8080/sum?x=1&y=2
+    @GetMapping("/sum")
+    fun sum(
+        @RequestParam(value = "x") x: Int,
+        @RequestParam(value = "y") y: Int
+    ) = x + y
+}
