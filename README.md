b9b00f99607a18dd4e48f5788b8754355e7313a8
Thiago Resende <thiago.gcresende@gmail.com>
2024-11-24 10:16:37 -0300

    update Kubernetes configurations and enhance CPU stress testing in calculator API

--- README.md
+++ README.md
@@ -13,6 +13,7 @@ docker run -p 8080:8080 --rm tresende/calculator-api
 
 3- create deployment
 ```bash
+k create ns calculator
 k apply -f calculator-api-deployment.yaml
 ```
 
@@ -20,4 +21,14 @@ k apply -f calculator-api-deployment.yaml
 
 ```bash
 k apply -f calculator-api-service.yaml
+```
+
+5- open dashboard
+```bash
+kubectl -n default port-forward svc/kubernetes-dashboard-1732394517-kong-proxy 8443:443
+```
+
+6- create token 
+```bash
+kubectl -n default create token admin-user 
 ```
--- calculator-api/src/main/kotlin/com/tresende/calculatorapi/CalculatorApiApplication.kt
+++ calculator-api/src/main/kotlin/com/tresende/calculatorapi/CalculatorApiApplication.kt
@@ -7,5 +7,25 @@ import org.springframework.boot.runApplication
 class CalculatorApiApplication
 
 fun main(args: Array<String>) {
+    stressCpu()
     runApplication<CalculatorApiApplication>(*args)
 }
+
+private fun stressCpu() {
+    // Simulando uso intenso de CPU
+    println("Iniciando o stress na CPU...")
+    for (i in 0 until 400) {
+        Thread { doCpuIntensiveWork() }.start()
+    }
+}
+
+private fun doCpuIntensiveWork() {
+    var result = 0.0
+    while (true) {
+        for (i in 0 until 1_000_000) {
+            result += Math.sqrt(i.toDouble()) // operação pesada
+        }
+    }
+}
+
+// docker build . -t tresende/calculator-api &&  docker tag tresende/calculator-api latest && docker push tresende/calculator-api
